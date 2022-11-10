f2d7e621af663b24c9cd392ea1ba0647bf2879b5
Anton Berisha <43340489+islo123@users.noreply.github.com>
2022-11-10 06:38:49 +0200

    Update data.js

--- src/data.js
+++ src/data.js
@@ -18,15 +18,6 @@ const people = [
       'Gastropub sustainable tousled prism occupy. Viral XOXO roof party brunch actually, chambray listicle microdosing put a bird on it paleo subway tile squid umami.',
   },
   {
-    id: 3,
-    image:
-      'https://res.cloudinary.com/diqqf3eq2/image/upload/v1595959121/person-1_aufeoq.jpg',
-    name: 'peter smith',
-    title: 'product designer',
-    quote:
-      'Drinking vinegar polaroid street art echo park, actually semiotics next level butcher master cleanse hammock flexitarian ethical paleo.',
-  },
-  {
     id: 4,
     image:
       'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883334/person-1_rfzshl.jpg',
